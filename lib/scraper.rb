acb3dc7e52333fb1d0f1f1eda4659fbb30862148
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-11 02:03:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,23 +27,31 @@ class Scraper
   def self.scrape_profile_page(profile_url)
       html = open(profile_url)
       studentpage=Nokogiri::HTML(html)
-    #  res = studentpage.css("body div.main-wrapper profile")
-    hash =
-     {
-
-   linkedin:  studentpage.css('body > div > div.vitals-container > div.social-icon-container > a:nth-child(2)').attribute('href').value,
-   github: studentpage.css('body > div > div.vitals-container > div.social-icon-container > a:nth-child(3)').attribute('href').value,
-   blog: 
-   bio: studentpage.css("body > div > div.details-container > div.bio-block.details-block > div > div.description-holder > p").text,
-   profile_quote: studentpage.css("body > div > div.vitals-container > div.vitals-text-container > div").text,
-   twitter: studentpage.css("body > div > div.vitals-container > div.social-icon-container > a:nth-child(1)").attribute('href').value
-     }
-
-
+      arr=[]
+      social = studentpage.css("div.social-icon-container a")
+      hash={}
+  social.each do |child|
+      value  = child.attribute('href').value
+  if value.include? "linkedin"
+      hash[:linkedin] = value
+  elsif  value.include? "github"
+     hash[:github] =  value
+  elsif  value.include? "twitter"
+     hash[:twitter] = value
+   else
+        hash[:blog] =  value
   end
 
+ end
+    hash[:bio] = studentpage.css("body > div > div.details-container > div.bio-block.details-block > div > div.description-holder > p").text
+    hash[:profile_quote] = studentpage.css("body > div > div.vitals-container > div.vitals-text-container > div").text
+    hash
+  end
 
 end
+
+#https://learn-co-curriculum.github.io/student-scraper-test-page/students/sean-liu.html
+#binding.pry
 #https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html
 #https://learn-co-curriculum.github.io/student-scraper-test-page/students/eric-an.html
 #html="https://learn-co-curriculum.github.io/student-scraper-test-page/"
--- lib/student.rb
+++ lib/student.rb
@@ -5,16 +5,24 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-
+     student_hash.each do |key ,value|
+      self.send(("#{key}="),value)
+    #  binding.pry
+    end
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-
+      students_array.each do |hash|
+         self.new(hash)
+         #binding.pry
+      end
   end
 
   def add_student_attributes(attributes_hash)
-
+    attributes_hash.each { |key,value|
+        self.send(("#{key}="),value)
+                         }
   end
 
   def self.all
